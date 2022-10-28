620998429d3c00c3333c03c73d8dc7c75c04ed40
Vitalik <vitalikvlasenko88@gmail.com>
2022-10-29 01:03:57 +0300

    add a lot of game function

--- src/hangman/Hangman.java
+++ src/hangman/Hangman.java
@@ -1,5 +1,6 @@
 package hangman;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Hangman {
     private String[] words = {"ant", "baboon", "badger", "bat", "bear", "beaver", "camel",
@@ -13,74 +14,90 @@ public class Hangman {
             "wombat", "zebra"};
 
 
-    private String[] gallows = {
-            "+---+\n" +
-                    "|   |\n" +
-                    "    |\n" +
-                    "    |\n" +
-                    "    |\n" +
-                    "    |\n" +
-                    "=========\n",
+    //if you were wondering, the only way to print '\' is with a trailing escape character, which also happens to be '\'
+    private String[] gallows = new String[]{
+            """
++---+
+|   |
+    |
+    |
+    |
+    |
+=========
+""",
 
-            "+---+\n" +
-                    "|   |\n" +
-                    "O   |\n" +
-                    "    |\n" +
-                    "    |\n" +
-                    "    |\n" +
-                    "=========\n",
+            """
++---+
+|   |
+O   |
+    |
+    |
+    |
+=========
+""",
 
-            "+---+\n" +
-                    "|   |\n" +
-                    "O   |\n" +
-                    "|   |\n" +
-                    "    |\n" +
-                    "    |\n" +
-                    "=========\n",
+            """
++---+
+|   |
+O   |
+|   |
+    |
+    |
+=========
+""",
 
-            " +---+\n" +
-                    " |   |\n" +
-                    " O   |\n" +
-                    "/|   |\n" +
-                    "     |\n" +
-                    "     |\n" +
-                    " =========\n",
+            """
+ +---+
+ |   |
+ O   |
+/|   |
+     |
+     |
+ =========
+""",
 
-            " +---+\n" +
-                    " |   |\n" +
-                    " O   |\n" +
-                    "/|\\  |\n" + //if you were wondering, the only way to print '\' is with a trailing escape character, which also happens to be '\'
-                    "     |\n" +
-                    "     |\n" +
-                    " =========\n",
+            """
+ +---+
+ |   |
+ O   |
+/|\\  |
+     |
+     |
+ =========
+""",
 
-            " +---+\n" +
-                    " |   |\n" +
-                    " O   |\n" +
-                    "/|\\  |\n" +
-                    "/    |\n" +
-                    "     |\n" +
-                    " =========\n",
+            """
+ +---+
+ |   |
+ O   |
+/|\\  |
+/    |
+     |
+ =========
+""",
 
-            " +---+\n" +
-                    " |   |\n" +
-                    " O   |\n" +
-                    "/|\\  |\n" +
-                    "/ \\  |\n" +
-                    "     |\n" +
-                    " =========\n"};
+            """
+ +---+
+ |   |
+ O   |
+/|\\  |
+/ \\  |
+     |
+ =========
+"""};
 
 
     private String wordToGuess;
     private char[] guessedChars;
-    private int gallowsPosition;
     private char[] missedChars;
+    private int gallowsPosition;
     private char prevGuessingChar;
 
 
     public Hangman() {
         Random r = new Random();
-        this.wordToGuess = words[r.nextInt(words.length)];
+//        this.wordToGuess = words[r.nextInt(words.length)];
+        this.wordToGuess = "wall";
         this.guessedChars = new char[wordToGuess.length()];
         this.gallowsPosition = 0;
         this.missedChars = new char[6];
@@ -132,25 +149,104 @@ public class Hangman {
 
     private void printMisses() {
         System.out.print("Misses:  ");
-        if (this.missedChars[0] == 0) return;
+        if (this.missedChars[0] == 0) {
+            System.out.println();
+            return;
+        }
 
-        for (int i = 0; i < this.missedChars.length; i++) {
-            System.out.print(this.missedChars[i]);
+        for (char missedChar : this.missedChars) {
+            System.out.print(missedChar);
         }
         System.out.println();
     }
 
 
-    private void clearScreen() {
+    private void cleanScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
 
 
+    private boolean checkCharInWord(char in) {
+        for (int i = 0; i < this.wordToGuess.length(); i++) {
+            if (this.wordToGuess.charAt(i) == in) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    private void addCharToGuessedArr(char c) {
+        for (int i = 0; i < this.guessedChars.length; i++) {
+            if (this.guessedChars[i] == 0) {
+                this.guessedChars[i] = c;
+                return;
+            }
+        }
+    }
+
+
+    private void addCharToMissedArr(char c) {
+        for (int i = 0; i < this.missedChars.length; i++) {
+            if (this.missedChars[i] == 0) {
+                this.missedChars[i] = c;
+                return;
+            }
+        }
+    }
+
+
+    private void inputGuessChar() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Guess: ");
+        char tmpInput = scanner.next().charAt(0);
+        if (checkCharInWord(tmpInput)) {
+            addCharToGuessedArr(tmpInput);
+        } else {
+            addCharToMissedArr(tmpInput);
+            this.gallowsPosition++;
+        }
+        prevGuessingChar = tmpInput;
+    }
+
+
+    private void printGameResult() {
+        if (this.gallowsPosition == this.gallows.length - 1) {
+            printGallows();
+            System.out.println("\nRIP!\nThe word was: '" + this.wordToGuess + "'");
+        } else {
+            printPrevGuessingChar();
+            printGallows();
+            printWord();
+            System.out.println("\nGOOD WORK!");
+        }
+    }
+
+
+//    private boolean checkWordWasGuessed() {
+//        for (int i = 0; i < this.guessedChars.length; i++) {
+//            if (isContain(guessedChars[i]))
+//        }
+//    }
+
+
     public void startGame() {
-        printPrevGuessingChar();
-        printGallows();
-        printWord();
-        printMisses();
+        /*
+        1) решить как ретёрнуться с цикла ака победа
+        2) решить проблему с переполнением this.guessedChars
+         */
+
+        while (this.gallowsPosition != this.gallows.length - 1 ) {
+            printPrevGuessingChar();
+            printGallows();
+            printWord();
+            System.out.println();
+            printMisses();
+            System.out.println();
+            inputGuessChar();
+            cleanScreen();
+        }
+        printGameResult();
     }
 }
