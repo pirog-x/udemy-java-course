e7c1c9cc9a97bba4a5ebe78726d0a545157bb765
Vitalik <vitalikvlasenko88@gmail.com>
2022-11-02 16:06:36 +0200

    done Vending Machine – Part 2

--- src/module2/p7/vendingMachine/Machine.java
+++ src/module2/p7/vendingMachine/Machine.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 class Machine {
     private Item[][] items;
     
@@ -19,13 +17,59 @@ class Machine {
 
 
     // getters
-    public Item getItem(int column, int row) {
+    public Item getItem(int row, int column) {
+        if (items.length <= row || row < 0) return null;
+        if (items[row].length <= column || column < 0) return null;
+        
         return new Item(items[row][column]);
     }
 
+    public double getItemsPrice(int row, int column) {
+        if (items.length <= row || row < 0) return -1;
+        if (items[row].length <= column || column < 0) return -1;
+
+        return items[row][column].getPrice();
+    }
+
+    public int getItemsQuantity(int row, int column) {
+        if (items.length <= row || row < 0) return -1;
+        if (items[row].length <= column || column < 0) return -1;
+
+        return items[row][column].getQuantity();
+    }
+
+    public String getItemsName(int row, int column) {
+        if (items.length <= row || row < 0) return "NaN";
+        if (items[row].length <= column || column < 0) return "NaN";
+
+        return items[row][column].getName();
+    }
+
+
     // setters
-    public void setItem(Item item, int column, int row) {
+    public void setItem(Item item, int row, int column) {
         if (item == null) return;
         items[row][column] = new Item(items[row][column]);
+    }
+
+    public void setItemsName(String name, int row, int column) {
+        this.items[row][column].setName(name);
+    }
+
+    public void setItemsPrice(double price, int row, int column) {
+        this.items[row][column].setPrice(price);
+    }
+
+    public void setItemsQuantity(int quantity, int row, int column) {
+        this.items[row][column].setQuantity(quantity);
+    }
+
+
+    // methods
+    public boolean dispense(int row, int column) {
+        if (items[row][column].getQuantity() <= 0) return false;
+        
+        items[row][column].setQuantity(items[row][column].getQuantity() - 1);
+        return true;
     }
 };
