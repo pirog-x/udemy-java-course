abe19b1d7a4b913f71e5ed7b4c39d6222137ba11
pirog <vitalikvlasenko88@gmail.com>
2022-11-21 20:45:33 +0200

    done Movie Store – Part 4

--- module2/p8/movie-store/models/Store.java
+++ module2/p8/movie-store/models/Store.java
@@ -11,6 +11,9 @@ public class Store {
     }
 
     public Movie getMovie(int index) {
+        if (movies.isEmpty()) {
+            throw new IllegalStateException("store is empty");
+        }
         return new Movie(movies.get(index));
     }
 
@@ -26,15 +29,23 @@ public class Store {
         if (name.isBlank() || action.isBlank()) {
             throw new IllegalArgumentException("name/action cannot be blank/null");
         }
+        if (movies.isEmpty()) {
+            throw new IllegalStateException("store is empty");
+        }
 
         for (Movie m : movies) {
             if (m.getName().equals(name)) {
                 if (action.equalsIgnoreCase("sell")) {
                     movies.remove(m);
                 } else if (action.equalsIgnoreCase("rent")) {
+                    if (!m.isAvailable()) {
+                        throw new IllegalStateException("movie was sold");
+                    }
                     m.setAvailable(false);
                 } else if (action.equalsIgnoreCase("return")) {
                     m.setAvailable(true);
+                } else {
+                    throw new IllegalArgumentException("wrong action");
                 }
                 break;
             }
