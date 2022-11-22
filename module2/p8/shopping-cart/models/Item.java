25040a9750234e3b1c836f95f43bce538ff1a67d
pirog <vitalikvlasenko88@gmail.com>
2022-11-22 15:53:10 +0200

    done Shopping Cart – Part 2

---
+++ module2/p8/shopping-cart/models/Item.java
@@ -0,0 +1,48 @@
+package models;
+
+public class Item {
+    private String name;
+    private double price;
+
+    public Item(String name, double price) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("name cannot be null/blank");
+        }
+        if (price < 0) {
+            throw new IllegalArgumentException("price cannot be less than zero");
+        }
+        this.name = name;
+        this.price = price;
+    }
+
+    public Item(Item item) {
+        this.name = item.name;
+        this.price = item.price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("name cannot be null/blank");
+        }
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("price cannot be less than zero");
+        }
+        this.price = price;
+    }
+
+    public String toString() {
+        return name + ": $" + price + " ";
+    }
+}
---
+++ module2/p8/shopping-cart/models/Store.java
@@ -0,0 +1,7 @@
+package models;
+
+import java.util.List;
+
+public class Store {
+    private List<Item> items;
+}
