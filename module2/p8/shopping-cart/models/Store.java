341aeb4dfdd6a01eda718e8afc0972be80de595f
pirog <vitalikvlasenko88@gmail.com>
2022-11-23 17:00:05 +0200

    done Shopping Cart – Part 7

--- module2/p8/shopping-cart/models/Store.java
+++ module2/p8/shopping-cart/models/Store.java
@@ -6,6 +6,7 @@ public class Store {
     public Store() {
         this.items = new Item[7][3];
     }
+
     public Item getItem(int row, int column) {
         return new Item(items[row][column]);
     }
